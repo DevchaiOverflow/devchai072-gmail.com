f959a4c9318a822a860cf8036851a0c28d625eee
Devchai072 <devchai072@gmail.com>
2020-03-04 10:39:13 +0700

    commit change

--- src/Maps/MapManager.js
+++ src/Maps/MapManager.js
@@ -11,28 +11,46 @@ const MapManager = props => {
     })
 
     const createMap = () => {
+        
         // lazy load the required ArcGIS API for JavaScript modules and CSS
-        EsriLoader.loadModules(['esri/Map', 'esri/views/MapView'], { css: true })
-        .then(([ArcGISMap, MapView]) => {
-            const map = new ArcGISMap({
-                basemap: 'topo-vector'
+        EsriLoader.loadModules([
+                'esri/Map',
+                'esri/views/MapView',
+                "esri/widgets/BasemapToggle",
+                "esri/widgets/BasemapGallery"
+            ], { css: true }
+        )
+        .then(([ArcGISMap, MapView, BasemapToggle, BasemapGallery]) => {
+
+            var map = new ArcGISMap({
+                basemap: "streets-navigation-vector"
             })
 
-            // load the map view at the ref's DOM node
-            const view = new MapView({
+            var view = new MapView({
                 container: mapRef.current,
                 map: map,
-                center: [-118, 34],
+                center: [-118.80500, 34.02700], // longitude, latitude
                 zoom: 13
             })
 
-            return () => {
-                if (view) {
-                    // destroy the map view
-                    view.container = null
+            var basemapToggle = new BasemapToggle({
+                view: view,
+                nextBasemap: "satellite"
+            })
+            view.ui.add(basemapToggle, "bottom-right")
+
+            var basemapGallery = new BasemapGallery({
+                view: view,
+                source: {
+                  portal: {
+                    url: "https://www.arcgis.com",
+                    useVectorBasemaps: true  // Load vector tile basemaps
+                  }
                 }
-            }
+            })
+            view.ui.add(basemapGallery, "bottom-right")
         })
+
     }
 
     return <MapManagerView 
