5aebbdd85b6db9b9b9c9d4b9dcdb09469976ba32
Devchai072 <devchai072@gmail.com>
2020-03-26 07:22:03 +0700

    Updates

---
+++ precache-manifest.23d636c94b76aded5cffc63304f3697e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45f2baa937c3d8e5ab1adcf132cd94a5",
+    "url": "/react-map/index.html"
+  },
+  {
+    "revision": "b318a2b143d1d93a31f4",
+    "url": "/react-map/static/css/main.2a2c8fd4.chunk.css"
+  },
+  {
+    "revision": "83d11b95e5ff4074f2e2",
+    "url": "/react-map/static/js/2.b5bd3dcc.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/react-map/static/js/2.b5bd3dcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b318a2b143d1d93a31f4",
+    "url": "/react-map/static/js/main.1ecb83b4.chunk.js"
+  },
+  {
+    "revision": "a9838c23d15751050086",
+    "url": "/react-map/static/js/runtime-main.c158cb83.js"
+  }
+]);
